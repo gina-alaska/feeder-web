a555273bb3f792ad374881bba30e72efbd92759a
Will Fisher <will@teknofire.net>
2014-07-17 16:01:20 -0800

    better listing of slideshows and validations

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -2,6 +2,8 @@ class Highlight < ActiveRecord::Base
   belongs_to :user
   has_one :entry
   
+  validates :description, presence: true, length: { in: 6..140 }
+  
   def to_s
     self.description
   end
