bcce9d5745932c1b5fd1f31e59c26c298d558466
Will Fisher <will@teknofire.net>
2014-08-06 13:23:32 -0800

    fixing issues with the siding and highlights list

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,9 @@
 module FeedsHelper
+  def feeds_like(source)
+    if source.is_a? Feed
+      Feed.order(title: :asc)
+    elsif source.is_a? Slideshow
+      Slideshow.order(title: :asc)
+    end
+  end
 end
